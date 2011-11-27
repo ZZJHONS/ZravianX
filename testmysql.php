20595b3c965aaf83ae6ff49119b13236250e9971
ZZJHONS <zzjhons@gmail.com>
2011-11-27 15:21:08 +0100

    Delete more no needed files

--- testmysql.php
+++
@@ -1,7 +0,0 @@
-<?php
-	include_once ("GameEngine/config.php");
-	$link = mysql_connect(SQL_SERVER,SQL_USER,SQL_PASS); 
-	if (!$link) {die('Could not connect to MySQL: ' . mysql_error());} 
-	echo 'Connection OK';
-	mysql_close($link); 
-?>
